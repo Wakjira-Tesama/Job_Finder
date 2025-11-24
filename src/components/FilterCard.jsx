9523eb56fd1c6dc967d020f1d1d287e9c4ae83b6
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-24 20:55:54 +0300

    file added

--- src/components/FilterCard.jsx
+++ src/components/FilterCard.jsx
@@ -4,6 +4,21 @@ import { Label } from "./ui/label";
 import { useDispatch } from "react-redux";
 import { setSearchedQuery } from "@/redux/jobSlice";
 
+const fitlerData = [
+  {
+    fitlerType: "Location",
+    array: ["Delhi NCR", "Bangalore", "Hyderabad", "Pune", "Mumbai"],
+  },
+  {
+    fitlerType: "Industry",
+    array: ["Frontend Developer", "Backend Developer", "FullStack Developer"],
+  },
+  {
+    fitlerType: "Salary",
+    array: ["0-40k", "42-1lakh", "1lakh to 5lakh"],
+  },
+];
+
 const FilterCard = () => {
   const [selectedValue, setSelectedValue] = useState("");
   const dispatch = useDispatch();
