291924ed3ff1b2dbc3ca82c40a5985581766eda8
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-24 20:22:28 +0300

    category courses file

---
+++ vite-project/src/components/CategoryCarousel.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./ui/carousel";
+import { Button } from "./ui/button";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { setSearchedQuery } from "@/redux/jobSlice";
+
+const category = [
+  "Frontend Developer",
+  "Backend Developer",
+  "Data Science",
+  "Graphic Designer",
+  "FullStack Developer",
+];
+
+export default CategoryCarousel;
