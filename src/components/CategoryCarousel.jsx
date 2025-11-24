e4453ab860031c0f1bf4170b11017f01c4d5d224
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-24 20:53:08 +0300

    added course file

--- src/components/CategoryCarousel.jsx
+++ src/components/CategoryCarousel.jsx
@@ -19,4 +19,35 @@ const category = [
   "FullStack Developer",
 ];
 
+const CategoryCarousel = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const searchJobHandler = (query) => {
+    dispatch(setSearchedQuery(query));
+    navigate("/browse");
+  };
+
+  return (
+    <div>
+      <Carousel className="w-full max-w-xl mx-auto my-20">
+        <CarouselContent>
+          {category.map((cat, index) => (
+            <CarouselItem className="md:basis-1/2 lg-basis-1/3">
+              <Button
+                onClick={() => searchJobHandler(cat)}
+                variant="outline"
+                className="rounded-full"
+              >
+                {cat}
+              </Button>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+    </div>
+  );
+};
+
 export default CategoryCarousel;
