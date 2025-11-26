f9fbf8ef1a9c9daf94ca3dabf45f6797b14ffa43
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-26 22:10:29 +0300

    file edited

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -1,3 +1,12 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./shared/Navbar";
+import FilterCard from "./FilterCard";
+import Job from "./Job";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
+
+// const jobsArray = [1, 2, 3, 4, 5, 6, 7, 8];
+
 const Jobs = () => {
   const { allJobs, searchedQuery } = useSelector((store) => store.job);
   const [filterJobs, setFilterJobs] = useState(allJobs);
