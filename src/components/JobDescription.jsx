385917bfaab9f3ea70f590409da7b7058b40e8be
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-26 22:08:00 +0300

    file

--- src/components/JobDescription.jsx
+++ src/components/JobDescription.jsx
@@ -1,3 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { Badge } from "./ui/badge";
+import { Button } from "./ui/button";
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { APPLICATION_API_END_POINT, JOB_API_END_POINT } from "@/utils/constant";
+import { setSingleJob } from "@/redux/jobSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { toast } from "sonner";
+
 const JobDescription = () => {
   const { singleJob } = useSelector((store) => store.job);
   const { user } = useSelector((store) => store.auth);
