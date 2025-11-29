cde11e93f37435d426b906de18cc8e142a7ddcd3
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-11-29 21:43:56 +0300

    file edited

--- src/components/admin/AdminJobs.jsx
+++ src/components/admin/AdminJobs.jsx
@@ -1,3 +1,13 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "../shared/Navbar";
+import { Input } from "../ui/input";
+import { Button } from "../ui/button";
+import { useNavigate } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import AdminJobsTable from "./AdminJobsTable";
+import useGetAllAdminJobs from "@/hooks/useGetAllAdminJobs";
+import { setSearchJobByText } from "@/redux/jobSlice";
+
 const AdminJobs = () => {
   useGetAllAdminJobs();
   const [input, setInput] = useState("");
