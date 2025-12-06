71870bcf7b29e8153fad94180fc9e7d9f7826e2d
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-12-06 19:07:58 +0300

    file add its import method

--- src/components/admin/ProtectedRoute.jsx
+++ src/components/admin/ProtectedRoute.jsx
@@ -1,3 +1,7 @@
+import { useEffect } from "react";
+import { useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+
 const ProtectedRoute = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
 
