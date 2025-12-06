04bc714351d9b3c74f7be769924e1c23b6b5a8ed
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-12-06 19:06:44 +0300

    file is edited

--- src/components/admin/ProtectedRoute.jsx
+++ src/components/admin/ProtectedRoute.jsx
@@ -1,7 +1,3 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
-
 const ProtectedRoute = ({ children }) => {
   const { user } = useSelector((store) => store.auth);
 
