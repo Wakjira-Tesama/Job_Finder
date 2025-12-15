5caa3d7a2a7e4ec2ea8d830e68eeeea7c3b49bdb
Wakjira-Tesama <wakjiratesama@gmail.com>
2025-12-15 23:32:07 +0300

    added

---
+++ src/main.jsx
@@ -0,0 +1,12 @@
+const persistor = persistStore(store);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+        <Toaster />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
